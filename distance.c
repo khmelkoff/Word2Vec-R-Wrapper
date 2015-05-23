626710946d974499ba40a948f687d6797346ba52
khmelkoff <khmelkoff@gmail.com>
2015-05-23 16:10:03 +0300

    del

--- distance.c
+++
@@ -1,8 +0,0 @@
-
-#include "distance.h"
-
-void CWrapper_distance(char **file_name, char **word, char **returnw, double *returnd, char **size)
-{
-    distance(*file_name, *word, *returnw, returnd, *size);
-}
-
